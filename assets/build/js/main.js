53ba1f878c30e8dcf934c0fbe1a183f2ce207c3b
ivelebedev <ivelebedev@gmail.com>
2019-03-05 13:45:22 +0300

    init

---
+++ assets/build/js/main.js
@@ -0,0 +1,61 @@
+//SLICK-SLIDER
+$(document).ready(function() {
+    $('.slick-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+    });
+	
+
+});
+
+// Popup READ-MORE
+$(document).ready(function($){
+    //�������� read-more-popup
+    $('.read-more').on('click', function(event){
+        event.preventDefault();
+        $('.read-more-popup').addClass('read-more-show');
+    });
+    //�������� read-more-popup
+    $('.close-read-more').on('click', function(){
+
+            event.preventDefault();
+            $('.read-more-popup').removeClass('read-more-show');
+        // }
+    });
+    //������� ��� ������� esc �� ����������
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+            $('.read-more-popup').removeClass('read-more-show');
+        }
+    });
+}); 
+
+//Popup LOGIN
+$(document).ready(function($){
+    //�������� ����� �����
+    $('#login').on('click', function(event){
+        event.preventDefault();
+        $('.login-popup').addClass('show');
+    });
+    //�������� read-more-popup
+    $('.close-login').on('click', function(){
+            event.preventDefault();
+            $('.login-popup').removeClass('show');
+        // }
+    });
+    //������� ��� ������� esc �� ����������
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+            $('.login-popup').removeClass('show');
+        }
+    });
+}); 
+
+//��������� �� ����� � ����� ������� ����� ������
+$(document).click(function(e) {
+    //if you click on anything except the modal itself or the "open modal" link, close the modal
+    if (!$(e.target).closest(".login-popup, #login").length) {
+        $('.login-popup').removeClass('show');
+    }
+  });
---
+++ assets/src/js/main.js
@@ -0,0 +1,61 @@
+//SLICK-SLIDER
+$(document).ready(function() {
+    $('.slick-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+    });
+	
+
+});
+
+// Popup READ-MORE
+$(document).ready(function($){
+    //Открытие read-more-popup
+    $('.read-more').on('click', function(event){
+        event.preventDefault();
+        $('.read-more-popup').addClass('read-more-show');
+    });
+    //Закрытие read-more-popup
+    $('.close-read-more').on('click', function(){
+
+            event.preventDefault();
+            $('.read-more-popup').removeClass('read-more-show');
+        // }
+    });
+    //Закрыть при нажатии esc на клавиатуре
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+            $('.read-more-popup').removeClass('read-more-show');
+        }
+    });
+}); 
+
+//Popup LOGIN
+$(document).ready(function($){
+    //Открытие блока входа
+    $('#login').on('click', function(event){
+        event.preventDefault();
+        $('.login-popup').addClass('show');
+    });
+    //Закрытие read-more-popup
+    $('.close-login').on('click', function(){
+            event.preventDefault();
+            $('.login-popup').removeClass('show');
+        // }
+    });
+    //Закрыть при нажатии esc на клавиатуре
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+            $('.login-popup').removeClass('show');
+        }
+    });
+}); 
+
+//Закрываем по клику в любой области кроме попапа
+$(document).click(function(e) {
+    //if you click on anything except the modal itself or the "open modal" link, close the modal
+    if (!$(e.target).closest(".login-popup, #login").length) {
+        $('.login-popup').removeClass('show');
+    }
+  });
